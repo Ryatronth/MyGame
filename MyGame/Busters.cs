462fa5cfc391c5a1d347e97755ef61eb6f722656
Ryatronth <119940759+Ryatronth@users.noreply.github.com>
2023-04-17 20:54:41 +0500

    Add files via upload

---
+++ MyGame/Busters.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace MyGame
+{
+    class Busters
+    {
+        public static Control GetSpeedBuster(PictureBox enemie)
+        {
+            var p = Form.ActiveForm;
+            PictureBox speedBust = new PictureBox();
+            speedBust.Location = new Point(enemie.Location.X, enemie.Location.Y);
+            speedBust.Size = new Size(50, 50);
+            speedBust.BackgroundImage = Properties.Resources.speed;
+            speedBust.BackgroundImageLayout = ImageLayout.Stretch;
+            speedBust.Name = "SpeedBuster";
+            return speedBust;
+        }
+
+        public static void MoveBusters()
+        {
+            var p = Form.ActiveForm;
+            foreach (Control buster in p.Controls)
+            {
+                if (buster.Name.Contains("Buster"))
+                {
+                    buster.Top += 3;
+                }
+            }
+        }
+
+        public static void GetTimeBust()
+        {
+            var p = Form.ActiveForm;
+            foreach (Control buster in p.Controls)
+            {
+                if (buster.Name.Contains("Buster"))
+                {
+                    foreach (Control player in p.Controls)
+                    {
+                        if (player.Name == "player")
+                        {
+                            if (buster.Bounds.IntersectsWith(player.Bounds))
+                            {
+                                if (buster.Name.Contains("Speed"))
+                                {
+                                    AlienShooter.jump += 3;
+                                }                                   
+                                p.Controls.Remove(buster);
+
+                                AlienShooter.points += 5;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MyGame/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Resources;
+using System.Runtime.Remoting.Lifetime;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MyGame
+{
+    public partial class AlienShooter : Form
+    {
+        public AlienShooter()
+        {                    
+            InitializeComponent();
+
+            new Enemie().CreateSprites(this);
+            player = new Player().CreateSprites(this);
+        }
+
+        public static PictureBox player;
+
+        bool moveLeft, moveRight, moveTop, moveBottom, rocket;      
+
+        public static int jump = 3;
+
+        public static int points = 0;      
+
+        public void Score(int points)
+        {
+            Counter.Text = "Очки: " + points.ToString();
+        }
+
+        private void GetScore(object sender, EventArgs e)
+        {
+            Score(points);
+        }      
+
+        private void MovePlayer(object sender, EventArgs e)
+        {
+            if (moveLeft == true && player.Left > 0)
+                player.Left -= jump;
+            if (moveRight == true && player.Right < 1600)
+                player.Left += jump;
+            if (moveTop == true && player.Top > 0)
+                player.Top -= jump;
+            if (moveBottom == true && player.Bottom < 815)
+                player.Top += jump;
+            Player.CrashPlayer();
+        }
+
+        private void TimeOfBusts(object sender, EventArgs e)
+        {
+            Busters.GetTimeBust();
+        }
+
+        private void RocketsBustersMove(object sender, EventArgs e)
+        {
+            Rocket.InteractionWithRocket();
+            Busters.MoveBusters();
+        }
+
+        private void KeysDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                moveLeft = true;
+            else if (e.KeyCode == Keys.D)
+                moveRight = true;
+            else if (e.KeyCode == Keys.W)
+                moveTop = true;
+
+            else if (e.KeyCode == Keys.S)
+                moveBottom = true;
+            else if (e.KeyCode == Keys.Space)
+            {
+                rocket = true;
+                Rocket.GetRocket();
+            }           
+        }
+
+        private void KeysUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                moveLeft = false;
+            else if (e.KeyCode == Keys.D)
+                moveRight = false;
+            else if (e.KeyCode == Keys.W)
+                moveTop = false;
+            else if (e.KeyCode == Keys.S)
+                moveBottom = false;
+            else if (e.KeyCode == Keys.Space)
+                rocket = false;
+        }
+
+        private void GetEnemieRocket(object sender, EventArgs e)
+        {
+            EnemieRocket.GetEnemieRocket();
+        }
+    }
+}
